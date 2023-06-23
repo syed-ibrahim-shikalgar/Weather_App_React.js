918f66e514aa2a8ac68368306cda67c435108d7b
syed ibrahim shikalgar <syedibrahim7755@gamail.com>
2023-06-23 17:49:48 +0530

    wind speed and coordeinates added

--- src/components/Wind.js
+++ src/components/Wind.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import "../css/style.css";
 import { Link } from "react-router-dom";
 
-const Weather = () => {
+const Wind = () => {
   const [city, setCity] = useState(null);
   const [search, setSearch] = useState("Pune");
 
   const fetchApi = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=2f83bf57758a27fd28acf86fc2dcb429`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=2f83bf57758a27fd28acf86fc2dcb429`;
     const response = await fetch(url);
     const resJson = await response.json();
-    setCity(resJson.main);
+    setCity(resJson.wind);
   };
   useEffect(() => {
     fetchApi();
@@ -36,16 +36,13 @@ const Weather = () => {
               <ion-icon name="location-outline"></ion-icon>
               {search}
             </h2>
-            <h1 className="temp">{city.temp} °Cel</h1>
-            <h3 className="temp_min_max">
-              Min : {city.temp_min} °Cel | Max : {city.temp_max} °Cel
-            </h3>
+            <h1 className="temp">{city.speed} Km/h</h1>
           </div>
         )}
 
-        <div className="wind">
-          <Link to="/Wind">
-            <button className="windbtn"> Check Wind </button>
+        <div className="home">
+          <Link to="/">
+            <button className="homebtn"> Home </button>
           </Link>
           <Link to="/Coord">
             <button className="coord"> Co-ordinates </button>
@@ -56,4 +53,4 @@ const Weather = () => {
   );
 };
 
-export default Weather;
+export default Wind;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Wind from "./components/Wind.js";
+import Coordinate from "./components/Coordinate";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Wind",
+    element: <Wind />,
+  },
+  {
+    path: "/Coord",
+    element: <Coordinate />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
